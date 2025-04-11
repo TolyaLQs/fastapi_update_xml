8e2f7a9b446025cf2de5efb5cdb15c88276644c9
TolyaLQs <sdtolya@gmail.com>
2025-04-11 11:28:31 +0500

    new V 3.2.1

--- app4/routers/files.py
+++ app4/routers/files.py
@@ -1,28 +1,111 @@
 # routers/files.py
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, UploadFile, File, HTTPException, Depends
 import shutil
 import os
-import xml.etree.ElementTree as ET
+from lxml import etree as ET
+from routers.admin import get_db
+from sqlalchemy.orm import Session
+from models import Site, Category, Product, Description
+import itertools
 
 router = APIRouter()
 
 UPLOAD_DIR = "uploads"
+OUTPUT_DIR = "output"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
+def update_file(tree, site, db):
+    error_site = {'site_name': site.name,
+                  'check_error': False,
+                  'products_error': [],
+                  }
+    products_error = []
+    root = tree.getroot()
+    categories = db.query(Category).filter(Category.site_id == site.id).all()
+    products_dict = {}
+    for category in categories:
+        products = db.query(Product).filter(Product.category_id == category.id).all()
+        for product in products:
+            products_dict[product.name] = category.id
+    for shop in root:
+        for element in shop:
+            if element.tag == 'categories':
+                while len(element) > 0:
+                    element.remove(element[0])
+                for category in categories:
+                    if category.parent_id is None:
+                        cat = ET.SubElement(element, 'category', id=str(category.id))
+                        cat.text = category.name
+                    else:
+                        cat = ET.SubElement(element, 'category', id=str(category.id), parentId=str(category.parent_id))
+                        cat.text = category.name
+            #             ET.CDATA()
+            if element.tag == 'offers':
+                for offers in element:
+                    offer_id = offers.attrib['id']
+                    category_id = None
+                    offer_error = False
+                    offer_name = ''
+                    for offer in offers:
+                        if offer.tag == 'name':
+                            offer_name = offer.text
+                            for key, value in products_dict.items():
+                                if key in offer.text:
+                                    category_id = value
+                        if offer.tag == 'categoryId':
+                            if category_id is not None:
+                                offer.text = str(category_id)
+                            else:
+                                offer.text = ''
+                                offer_error = True
+                        if offer.tag == 'description':
+                            if offer.text is not None:
+                                offer.text = ET.CDATA(offer.text)
+                    if offer_name == '':
+                        offer_error = True
+                    context = {'offer_id': offer_id, 'offer_name': offer_name, 'offer_error': offer_error}
+                    products_error.append(context)
+    error_site['products_error'] = products_error
+    if len(products_error) > 0:
+        error_site['check_error'] = True
+
+    return tree, error_site
+
+
+def open_file(file_path):
+    with open(file_path, "r", encoding='utf-8') as f:
+        parser = ET.XMLParser(encoding="utf-8")
+        tree = ET.parse(source=f, parser=parser)
+        f.close()
+    return tree
+
+
 @router.post("/upload/")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
     try:
         file_path = os.path.join(UPLOAD_DIR, file.filename)
         with open(file_path, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
-            parser = ET.XMLParser(encoding="utf-8")
-            tree = ET.parse(source=file_path, parser=parser)
-            root = tree.getroot()
-            print(root)
             buffer.close()
-        otvet = update_file(root)
-        return {"filename": file.filename, "size": os.path.getsize(file_path)}
+        sites = db.query(Site).all()
+        report_list = []
+        if len(sites) == 0:
+            return {'status_code': 400, 'detail': 'Site not found'}
+        if len(sites) > 1:
+            for site in sites:
+                tree = open_file(file_path)
+                otvet, error = update_file(tree, site, db)
+                report_list.append(error)
+                file_save_path = OUTPUT_DIR+'/'+site.filename
+                otvet.write(file_save_path, encoding='utf-8')
+        else:
+            tree = open_file(file_path)
+            otvet, error = update_file(tree, sites, db)
+            report_list.append(error)
+            file_save_path = OUTPUT_DIR + '/' + sites.filename
+            otvet.write(file_save_path, encoding='utf-8')
+        return {"filename": file.filename, "size": os.path.getsize(file_path), 'report': report_list}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
--- app4/static/script.js
+++ app4/static/script.js
@@ -5,7 +5,7 @@ function uploadFile() {
     formData.append('file', file);
     uploadInput = document.getElementById('fileInput');
     uploadBtn = document.getElementById('uploadBtn');
-    load_file = document.querySelector('.load_file');
+    load_file = document.querySelector('.load-file');
     load_file.style.display = 'none';
     loading = document.querySelector('.loading');
     loading.style.display = 'flex';
@@ -18,28 +18,28 @@ function uploadFile() {
     .then(response => response.json())
     .then(data => {
         loading.style.display = 'none';
-        html = `<div class='info_file'>File ${data.filename} uploaded (${data.size} bytes)</div>
-                <div class="table_response">`;
+        html = `<div class='info-file'>File ${data.filename} uploaded (${data.size} bytes)</div>
+                <div class="table-response">`;
         data.report.forEach(site => {
             html += `<div class='site'>
-                <div class='site_title'>${site.site_name}</div>`;
+                <div class='site-title'>${site.site_name}</div>`;
             if (site.check_error === false){
-                html += `<div class='error_not'>Ошибок нет</div>`;
+                html += `<div class='error-not'>Ошибок нет</div>`;
             } else{
-                html += `<div class='error_yes'>Ошибки есть</div>`;
+                html += `<div class='error-yes'>Ошибки есть</div>`;
             }
             let i = 0;
             site.products_error.forEach(product =>{
                 i = i+1;
                 if (product.offer_error === false){
-                    html += `<div class='product error_not'>`;
+                    html += `<div class='product error-not'>`;
                 } else {
-                    html += `<div class='product error_yes'>`;
+                    html += `<div class='product error-yes'>`;
                 }
 
-                html += `<span class="product_number">${i}</span>
-                    <span class="product_id">id:${product.offer_id}</span>
-                    <span class="product_name">name:${product.offer_name}</span>`;
+                html += `<span class="product-number">${i}</span>
+                    <span class="product-id">id:${product.offer_id}</span>
+                    <span class="product-name">name:${product.offer_name}</span>`;
                 html += `</div>`;
             })
             html += `</div>`;
@@ -86,7 +86,7 @@ function createSite() {
                     contentType: 'application/json',
                     data: JSON.stringify(arr),
                     success: function (response) {
-                        if (response['status_code'] == 200){
+                        if (response['status-code'] == 200){
                             console.log(response['detail']);
                             loadSites();
                         } else{
@@ -195,10 +195,10 @@ function updateCategoryBtn(id) {
     let parent_id = document.querySelector('.category-parent-id-' + id).textContext;
     let product = document.querySelector('.category-product-' + id).textContent;
     const description = document.querySelector('.category-description-' + id).textContent;
-//    document.querySelector('.category-name-' + id).innerHTML = `<input class='input-category-name-${id}' name='input-category-name' value='${name}'></input>`;
+    document.querySelector('.category-name-' + id).innerHTML = `<input class='input-category-name-${id}' name='input-category-name' value='${name}'></input>`;
     document.querySelector('.category-parent-id-' + id).innerHTML = `<input class='input-category-parent-id-${id}' name='input-category-parent-id' value='${parent-id}'></input>`;
-    document.querySelector('.category-product-' + id).innerHTML = `<input class='input-category-product-${id}' name='input-category-product' value='${product}'></input>`;
-    document.querySelector('.category-description-' + id).innerHTML = `<input class='input-category-description-${id}' name='input-category-description' value='${description}'></input>`;
+//    document.querySelector('.category-product-' + id).innerHTML = `<input class='input-category-product-${id}' name='input-category-product' value='${product}'></input>`;
+//    document.querySelector('.category-description-' + id).innerHTML = `<input class='input-category-description-${id}' name='input-category-description' value='${description}'></input>`;
     document.querySelector('.category-update-' + id).innerHTML = `<button onclick='updateCategory(${id});'>✏️</button>`;
     document.querySelector('.category-delete-' + id).innerHTML = `<button onclick='cancelUpdateCategoryBtn(${id});'>x</button>`;
 }
@@ -206,19 +206,80 @@ function updateCategoryBtn(id) {
 function cancelUpdateCategoryBtn(id) {
     document.querySelector('.category-name-' + id).innerHTML = document.querySelector('.input-category-name-' + id).value;
     document.querySelector('.category-parent-id-' + id).innerHTML = document.querySelector('.input-category-parent-id-' + id).value;
-    document.querySelector('.category-product-' + id).innerHTML = document.querySelector('.input-category-product-' + id).value;
-    document.querySelector('.category-description-' + id).innerHTML = document.querySelector('.input-category-description-' + id).value;
+//    document.querySelector('.category-product-' + id).innerHTML = document.querySelector('.input-category-product-' + id).value;
+//    document.querySelector('.category-description-' + id).innerHTML = document.querySelector('.input-category-description-' + id).value;
     document.querySelector('.category-update-' + id).innerHTML = `<button onclick='updateCategoryBtn(${id});'>✏️</button>`;
     document.querySelector('.category-delete-' + id).innerHTML = `<button onclick='deleteCategory(${id});'>-</button>`;
 }
 
 function updateCategory(id) {
+    const name = document.querySelector('.input-category-name-' + id).value;
+    const parent_id = document.querySelector('.input-category-parent-id-' + id).value;
+    const site_id = document.querySelector('.site-id').value;
+    arr = {name: name, parent_id: parent_id, site_id: site_id};
+    const response = $.ajax({
+                url: '/categories/' + id,
+                type: 'PUT',
+                contentType: 'application/json',
+                data: JSON.stringify(arr),
+                success: function (response) {
+                    if (response['status_code'] == 200){
+                        console.log(response['detail']);
+                        loadSites();
+                    } else{
+                        console.log(response['detail']);
+                    }
+                },
+                error: function () {
+                    console.log("error");
+                }
+            });
+}
 
+function deleteCategory(id){
+    const response = $.ajax({
+                url: '/categories/' + id,
+                type: 'DELETE',
+                success: function (response) {
+                    if (response['status_code'] == 200){
+                        console.log(response['detail']);
+                        loadSites();
+                    } else{
+                        console.log(response['detail']);
+                    }
+                },
+                error: function () {
+                    console.log("error");
+                }
+            });
 }
 
 function createProduct(id) {
-
+    div = document.querySelector('.div-category-product-'+id);
+    const name = document.querySelector('.new-product').value;
+    const category_id = id;
+    arr = {name: name, category_id: category_id};
+    const response = $.ajax({
+                url: '/products/',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(arr),
+                success: function (response) {
+                    if (response['status_code'] == 200){
+                        console.log(response['detail']);
+                        html = `<span class="category-product-${response.db_product.id}">${response.db_product.name}</span>,`
+                        div.prepend(html);
+                    } else{
+                        console.log(response['detail']);
+                    }
+                },
+                error: function () {
+                    console.log("error");
+                }
+            });
 }
+
+function updateProduct(id)
 
 function createDescription(id){
 
